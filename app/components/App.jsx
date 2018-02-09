ff4a5daba5464d3175e739ead16fd92a8a2b9d86
Leevi Heino <leeviheino@live.fi>
2018-02-09 11:24:06 +0200

    Demo code

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,58 +1,27 @@
 import React from 'react';
 import uuid from 'uuid';
-import Notes from './Notes';
+import connect from '../libs/connect';
+import Lanes from './Lanes';
+import LaneActions from '../actions/LaneActions';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      notes: [
-        {
-          id: uuid.v4(),
-          task: 'Learn React'
-        },
-        {
-          id: uuid.v4(),
-          task: 'Do laundry'
-        }
-      ]
-    }
-  }
-  render() {
-    const {notes} = this.state;
-
-    return (
-      <div>
-        <button onClick={this.addNote}>+</button>
-        <Notes notes={notes} onDelete={this.deleteNote} />
-      </div>
-    );
-  }
-  addNote = () => {
-    // It would be possible to write this in an imperative style.
-    // I.e., through `this.state.notes.push` and then
-    // `this.setState({notes: this.state.notes})` to commit.
-    //
-    // I tend to favor functional style whenever that makes sense.
-    // Even though it might take more code sometimes, I feel
-    // the benefits (easy to reason about, no side effects)
-    // more than make up for it.
-    //
-    // Libraries, such as Immutable.js, go a notch further.
-    this.setState({
-      notes: this.state.notes.concat([{
-        id: uuid.v4(),
-        task: 'New task'
-      }])
+const App = ({LaneActions, lanes}) => {
+  const addLane = () => {
+    LaneActions.create({
+      id: uuid.v4(),
+      name: 'New lane'
     });
-  }
-  deleteNote = (id, e) => {
-    // Avoid bubbling to edit
-    e.stopPropagation();
+  };
 
-    this.setState({
-      notes: this.state.notes.filter(note => note.id !== id)
-    });
-  }
-}
+  return (
+    <div>
+      <button className="add-lane" onClick={addLane}>+</button>
+      <Lanes lanes={lanes} />
+    </div>
+  );
+};
+
+export default connect(({lanes}) => ({
+  lanes
+}), {
+  LaneActions
+})(App)
