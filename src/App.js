5259ddc03bf23db5270d5d74db5fd4269b87abaf
Leevi Heino <leeviheino@live.fi>
2018-02-22 00:44:30 +0200

    Title for location

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      weatherObservations: [{ id: 1, temperature: 0 }, { id: 2, temperature: 30 }]
+      weatherObservations: [
+        { id: 1, temperature: 0 },
+        { id: 2, temperature: 30 }
+      ]
     };
   }
 
@@ -35,6 +38,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <h1>Tokio (35.6584421,139.7328635)</h1>
         {this.state.weatherObservations.length === 0 && <p>Ei havaintoja</p>}
         {
           this.state.weatherObservations.map(
