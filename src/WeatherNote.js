434d32dbc54d6c20d89d23555bd5a48a7e66f9de
Leevi Heino <leeviheino@live.fi>
2018-02-22 19:56:44 +0200

    del

--- src/WeatherNote.js
+++ src/WeatherNote.js
@@ -2,15 +2,11 @@ import React, { Component } from 'react';
 
 class WeatherNote extends Component {
 
-  handleChange = (event) => {
-    // ...
-  }
-
   render() {
     return (
       <div className="weather-note">
         <input type="text"
-          value={this.props.temperature + ' (' + this.props.submissionTime + ')'} 
+          value={this.props.temperature + ' (' + this.props.submissionTime + ')'}
           onChange={this.handleChange} />
       </div>
     );
